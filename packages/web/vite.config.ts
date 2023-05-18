86798edf031d849d946ac596cd069b2a0046de93
Iqbal Maulana <iqbalmaulana@Iqbals-MacBook-Air.local>
2023-05-18 15:43:04 +0700

    adding tailwind

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,15 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import ViteTsConfigPathsPlugin from 'vite-tsconfig-paths';
-
 
 export default defineConfig({
-	plugins: [
-		sveltekit(), 
-		ViteTsConfigPathsPlugin({
-			root: '../../',
-		})
-	],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./packages/web/src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
