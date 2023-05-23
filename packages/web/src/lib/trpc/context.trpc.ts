fc8ed80dc966070f1d31b67659fb726384852477
IqbalLx <iqbal19600@gmail.com>
2023-05-23 08:43:38 +0700

    chore: missing adding file

--- packages/web/src/lib/trpc/context.trpc.ts
+++ packages/web/src/lib/trpc/context.trpc.ts
@@ -9,36 +9,3 @@ export async function createContext(event: RequestEvent) {
 }
 
 export type TRPCContext = inferAsyncReturnType<typeof createContext>;
-
-// import { Context } from '@migrasi/shared/entities';
-// import { authService } from '@migrasi/services/api/auth';
-// import { toTRPCError } from './trpc.util';
-// import { HTTPException } from '@migrasi/shared/errors';
-
-// if (authHeaderValue === undefined)
-// 		throw new TRPCError({
-// 			code: 'UNAUTHORIZED',
-// 			message: 'auth failed'
-// 		});
-
-// 	const token = authHeaderValue?.split(' ')[1];
-// 	if (token === undefined)
-// 		throw new TRPCError({
-// 			code: 'UNAUTHORIZED',
-// 			message: 'auth failed'
-// 		});
-
-// 	try {
-// 		const context: Context = await authService.authorize(token);
-
-// 		return context;
-// 	} catch (error) {
-// 		if (error instanceof HTTPException) throw toTRPCError(error);
-
-// 		console.error(error);
-// 		throw new TRPCError({
-// 			code: 'INTERNAL_SERVER_ERROR',
-// 			message: (error as Error).message,
-// 			cause: error as Error
-// 		});
-// 	}
